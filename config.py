ff9b814ae7e08cc9f5f5299ae7ad5b18a9ef6876
TheMrPhantom <schieljn@studi.informatik.uni-stuttgart.de>
2020-01-10 04:39:38 +0100

    Added more locations

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 areas=[
-    ["Flugzeug","Pilot","Fluggast","Tier in Box","Co-Pilot","Lotse","Fluggast","Flugbegleiter","Reisekranker Gast"],
-    ["Filmstudio","Regisseur","Statist","Konstümdesigner","Kameramann","Tontechniker","Stuntman","Schauspieler","Animateur"]
+    ["Flugzeug","Pilot","Fluggast","Tier in Box","Co-Pilot","Lotse","Fluggast","Flugbegleiter"],
+    ["Filmstudio","Regisseur","Statist","Konstümdesigner","Kameramann","Tontechniker","Stuntman","Schauspieler"],
+    ["Polizeistation","Strafverteidiger","Polizist","Archivar","Kriminalbeamter","Pressesprecher","Gefangener","Ermittler"],
+    ["Strand","Taucher","Urlauber","Rettungsschwimmer","Strandverkäufer","Animateur","Surfer","Dieb"],
+    ["Supermarkt","Regalbetreuer","Kassierer","Filialleiter","Fleischermeister","Ladendetektiv","Kunde","Praktikant"]
 ]
