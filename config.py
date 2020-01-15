609c8f74bcdb135551412eef9cda51a186be7614
TheMrPhantom <schieljn@studi.informatik.uni-stuttgart.de>
2020-01-15 22:36:33 +0100

    Added more places

--- config.py
+++ config.py
@@ -3,5 +3,29 @@ areas=[
     ["Filmstudio","Regisseur","Statist","Konstümdesigner","Kameramann","Tontechniker","Stuntman","Schauspieler",{'category':None}],
     ["Polizeistation","Strafverteidiger","Polizist","Archivar","Kriminalbeamter","Pressesprecher","Gefangener","Ermittler",{'category':None}],
     ["Strand","Taucher","Urlauber","Rettungsschwimmer","Strandverkäufer","Animateur","Surfer","Dieb",{'category':None}],
-    ["Supermarkt","Regalbetreuer","Kassierer","Filialleiter","Fleischermeister","Ladendetektiv","Kunde","Praktikant",{'category':None}]
+    ["Supermarkt","Regalbetreuer","Kassierer","Filialleiter","Fleischermeister","Ladendetektiv","Kunde","Praktikant",{'category':None}],
+    ["Militärbasis","Militärarzt","Oberst","Kantinenchef","Wachposten","Gefreiter","Deserteur","Unteroffizier",{'category':None}],
+    ["Werkstatt","Empfangsdame","Spengler","Sericetechniker","Geschäftsführer","KFZ-Mechaniker","Motoradfahrer","Autofahrer",{'category':None}],
+    ["Schule","Vertrauenslehrer","Schüler","Schulwart","Schulinspektor","Klassensprecher","Schuldirektor","Turnlehrer",{'category':None}],
+    ["Restaurant","Restaurantkritiker","Kellner","Sommelier","Pianist","Restaurantleiter","Gast","Küchenhilfe",{'category':None}],
+    ["Firmenfeier","Generaldirektor","Abteilungsleiter","DJ","Sektretärin","Großkunde","Partycrasher","Buchhalter",{'category':None}],
+    ["Botschaft","Diplomat","Flüchtling","Chauffeur","Sektretär","Botschafter","Sicherheitsbeamter","Tourist",{'category':None}],
+    ["Krankenhaus","Krankenschwester","Patient","Pfleger","Pathologe","Internist","Assistenzarzt","Chirurg",{'category':None}],
+    ["Flugzeug","Copilot","Pilot","Passagier Erster Klasse","Blinder Passagier","Flugingenieur","Flugbegleiter","Fluggast",{'category':None}],
+    ["Hotel","Hotelgast","Hoteldirektor","Zimmermädchen","Portier","Page","Rezeptionist","Barkeeper",{'category':None}],
+    ["Dampflokomotive","Speisewagenbegleiter","Schwarzfahrer","Schaffner","Fahrgast","Lokführer","Heizer","Grenzposten",{'category':None}],
+    ["U-Boot","Koch","Admiral","Maschinenarbeiter","Elektriker","Admiral","Funker","Navigationsoffizier",{'category':None}],
+    ["Theater","Schauspieler","Zuschauer","Bühnenarbeiter","Theaterdirektor","Platzeinweiser","Garderobier","Souffleur",{'category':None}],
+    ["Bank","Fillialleiter","Anlageberater","Räuber","Kunde","Wachmann","Kassier","Kreditberater",{'category':None}],
+    ["Wellness-Tempel","Kosmetiker","Masseur","Nageldesigner","Reinigungskraft","Stylist","Gast","Friseur",{'category':None}],
+    ["Kreuzfahrtschiff","Steward","Barkeeper","Kapitän","Reiseleiter","Musiker","Matrose","Passagier",{'category':None}],
+    ["Piratenschiff","Bootsmann","Geschützoffizier","Schiffsjunge","Steuermann","Koch","Kapitän","Gefangener",{'category':None}],
+    ["Zirkus","Jongleur","Hochseilakrobat","Feuerschluker","Messerwerfer","Dompteur","Zauberer","Clown",{'category':None}],
+    ["Casino","Sicherheitschef","Türsteher","Barkeeper","Spieler","Falschspieler","Croupier","Dealer",{'category':None}],
+    ["Weltraumstation","Arzt","Kosmonaut","Außerirdischer","Weltraumtourist","Bordingenieur","IT-Techniker","Wissenschaftler",{'category':None}],
+    ["Polarstation","Arzt","Expeditionsleiter","Hydrologe","Meteorologe","Funke","Geologe","Biologe",{'category':None}],
+    ["Universität","Bibliothek","Psychologe","Dekan","Studienberater","Tutor","Student","Professor",{'category':None}],
+    ["Supermarkt","Praktikant","Kunde","Ladendetektiv","Fleischermeister","Filialleiter","Kassier","Regalbetreuer",{'category':None}]
+
+
 ]
